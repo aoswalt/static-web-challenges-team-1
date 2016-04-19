1f0a363bdd2ff252bafa541da81cad7ebe41d80a
Adam Oswalt <adam.oswalt@gmail.com>
2016-04-19 13:50:06 -0500

    Create event listeners but broken listener binding

--- cards.js
+++ cards.js
@@ -9,3 +9,35 @@ function buildCard(text) {
       `<p>${text}</p>` +
     "</div>";
 }
+
+function addCard(text) {
+  var cardWrapper = document.getElementById("card-wrapper");
+  cardWrapper.innerHTML += buildCard(text);
+
+  var card = cardWrapper.children[cardWrapper.children.length - 1];
+  card.getElementsByClassName("bg-color")[0].onchange = setBgColor;
+  card.getElementsByClassName("fg-color")[0].onchange = setFgColor;
+  card.getElementsByClassName("delete-button")[0].onclick = deleteCard;
+}
+
+function setBgColor(event) {
+  //NOTE(adam): change event for color picker
+  event.srcElement.parentElement.style.backgroundColor = event.srcElement.value;
+}
+
+function setFgColor(event) {
+  //NOTE(adam): change event for color picker
+  event.srcElement.parentElement.style.color = event.srcElement.value;
+}
+
+function deleteCard(event) {
+  var cardContainer = document.getElementById("card-wrapper");
+  var card = event.srcElement.parentElement;
+  cardContainer.removeChild(card);
+}
+
+document.getElementById("create-button").onclick = function() {
+  var textArea = document.getElementById("card-text");
+  addCard(textArea.value);
+  textArea.value = "";
+};
